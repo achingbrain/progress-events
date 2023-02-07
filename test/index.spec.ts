4885ab8d48d4569be5d1b124e5ffec5adc09a58d
Alex Potsides <alex@achingbrain.net>
2023-02-07 08:10:49 +0100

    feat: initial implementation (#1)

---
+++ test/index.spec.ts
@@ -0,0 +1,16 @@
+/* eslint-env mocha */
+
+import { expect } from 'aegir/chai'
+import { CustomProgressEvent } from '../src/index.js'
+
+describe('progress', () => {
+  it('should have type and detail properties', async () => {
+    const type = 'event-type'
+    const detail = 'event-detail'
+
+    const event = new CustomProgressEvent<number>(type, detail)
+
+    expect(event).to.have.property('type', type)
+    expect(event).to.have.property('detail', detail)
+  })
+})
